dee971732fc6617193fe4bd4b156c6292af44603
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-11-23 23:45:41 +0100

    add client and server

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+
+const Navbar = () => {
+  return (
+    <div className="navbarContainer">
+      <div className="navbarLeft">
+        <span className="logo">Social Mediaaa</span>
+      </div>
+      <div className="navbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input
+            placeholder="Search for friends, posts or videos"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="navbarRight">
+        <span className="navbarLink">Homepage</span>
+        <span className="navbarLink">Timelineeee</span>
+        <div className="navbarIcons">
+          <div className="navbarIconItem">
+            <Person />
+            <span className="navbarIconBadge">1</span>
+          </div>
+          <div className="navbarIconItem">
+            <Chat />
+            <span className="navbarIconBadge">2</span>
+          </div>
+          <div className="navbarIconItem">
+            <Notifications />
+            <span className="navbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/" alt="" className="navbarImg" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ server/routes/posts.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+// POST create post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT update post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can only update your own posts");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE delete post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("The post has been deleted");
+    } else {
+      res.status(403).json("You can only delete your own posts");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT like/dislike post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("You like this post");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("You've removed your like from this post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET get post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get all posts
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
