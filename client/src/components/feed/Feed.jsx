bc7e300b749ce75d1cf585923bece817fdca41d6
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-11-25 23:50:27 +0100

    Map posts in feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Post from '../post/Post'
-import Share from '../share/Share'
+import React from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import { Posts } from "../../dummyData";
 
-import './feed.css'
+import "./feed.css";
 
 const Feed = () => {
-    return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share/>
-                <Post/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import { MoreVert, ThumbUpAltOutlined, FavoriteBorderOutlined } from '@material-ui/icons'
+import React from "react";
+import {
+  MoreVert,
+  ThumbUpAltOutlined,
+  FavoriteBorderOutlined,
+} from "@material-ui/icons";
+import { Users } from "../../dummyData";
 
-import './post.css'
+import "./post.css";
 
-const Post = () => {
-    return (
-        <div className="post" >
-            <div className="postWrapper">
-                <div className="postTop">
-                    <div className="postTopLeft">
-                        <img className="postProfileImg" src="/assets/ph.png" alt=""  />
-                        <span className="postUsername">Klaus</span>
-                        <span className="postDate">5 min ago</span>
-                    </div>
-                    <div className="postTopRight">
-                        <MoreVert/>
-                    </div>
-                </div>
-                <div className="postCenter">
-                    <span className="postText">Hey! Its my first post</span>
-                    <img className="postImg" src="/assets/ph.png" alt=""  />
-                </div>
-                <div className="postBottom">
-                    <div className="postBottomLeft">
-                        <ThumbUpAltOutlined htmlColor="#1877f2" className="likeIcon"/>
-                        <FavoriteBorderOutlined htmlColor="red" className="likeIcon"/>
-                        <span className="postLikeCounter">32 people likes this post</span>
-                    </div>
-                    <div className="postBottomRight">
-                        <span className="postCommentText">9 comments</span>
-                    </div>
-                </div>
-            </div>
+const Post = ({ post }) => {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img className="postProfileImg" src={Users.filter((u) => u.id === post.userId)[0].profilePicture} alt="" />
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-    )
-}
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <ThumbUpAltOutlined htmlColor="#1877f2" className="likeIcon" />
+            <FavoriteBorderOutlined htmlColor="red" className="likeIcon" />
+            <span className="postLikeCounter">{post.like}</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
