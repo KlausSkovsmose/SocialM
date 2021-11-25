a743cbdb1719bfed694f594b5e69c7fa24201ff1
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-11-25 23:06:45 +0100

    Add right side panel

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,13 +1,170 @@
-import React from 'react'
+import React from "react";
 
-import './rightbar.css'
+import "./rightbar.css";
 
 const Rightbar = () => {
-    return (
-        <div className="rightbar">
-            rightbar
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/ph.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Ajes</b> and <b>3 other friends</b> have birthday today
+          </span>
         </div>
-    )
-}
+        <img className="rightbarAd" src="/assets/ph.png" alt="" />
 
-export default Rightbar
+        <div className="onlineFriendsContainer">
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img
+                  className="rightbarProfileImg"
+                  src="/assets/ph.png"
+                  alt=""
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Ajes Nielsen</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
