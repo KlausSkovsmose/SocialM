76258808fb0e53bb9a064fe76644b08cef8a5d5d
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-05 23:50:51 +0100

    Fecth posts from api and map each one

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -5,23 +5,27 @@ import axios from "axios";
 
 import "./feed.css";
 
-const Feed = () => {
+const Feed = ({ username }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("posts/timeline/61ad25e165121f2422da19bd");
-      setPosts(res.data)
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/61ad25e165121f2422da19bd");
+      setPosts(
+        res.data
+      );
     };
     fetchPosts();
-  }, []);
+  }, [username]);
 
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
         {posts.map((p) => (
-          <Post key={p.id} post={p}/>
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
