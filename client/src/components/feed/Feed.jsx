edc589da4a9fdb71fb3ec540dd53878d9ca0dfe5
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-05 22:46:46 +0100

    Add timestamps to posts

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
-import { Posts } from "../../dummyData";
+import axios from "axios";
 
 import "./feed.css";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/61ad25e165121f2422da19bd");
+      setPosts(res.data)
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {posts.map((p) => (
           <Post key={p.id} post={p}/>
         ))}
       </div>
