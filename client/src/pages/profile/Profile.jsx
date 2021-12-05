40198088c5e9de2892a75b54a0702c5b243bbb8e
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-05 23:50:31 +0100

    Fetch user from api

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import axios from "axios";
 
 import "./profile.css";
 
 const Profile = () => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=klaus`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
   return (
     <>
       <Navbar />
@@ -19,13 +30,13 @@ const Profile = () => {
               <img className="profileUserImg" src={`${PF}ph.png`} alt="" />
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">My name</h4>
-              <span className="profileInfoDesc">hello everyone</span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed />
-            <Rightbar profile/>
+            <Feed username="klaus" />
+            <Rightbar user={user} />
           </div>
         </div>
       </div>
