08d7aeb23195e1ab45beda52691107cfaea7689d
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-12 14:36:05 +0100

    Add Context API

---
+++ client/src/apiCalls.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const loginCall = async (userCredentials, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await axios.post("auth/login", userCredentials);
+    dispatch({ type: "LOGIN_SUCCES", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAILURE", payload: err });
+  }
+};
