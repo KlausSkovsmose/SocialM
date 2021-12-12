bffdcfc32ad57d21de1c7f1bcf04ea26b7a50caa
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-12 15:59:14 +0100

    use context api on feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,24 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 import "./feed.css";
 
 const Feed = ({ username }) => {
   const [posts, setPosts] = useState([]);
+  const {user} = useContext(AuthContext)
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = username
         ? await axios.get("/posts/profile/" + username)
-        : await axios.get("posts/timeline/61ad25e165121f2422da19bd");
+        : await axios.get("posts/timeline/" + user._id);
       setPosts(
         res.data
       );
     };
     fetchPosts();
-  }, [username]);
+  }, [username, user._id]);
 
   return (
     <div className="feed">
