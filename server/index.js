16e7c5ae4b8431da5e91f38f39b1d6aaccf04fc1
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-12 22:31:36 +0100

    add multer to upload image locally

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path")
 
 dotenv.config();
 
@@ -18,14 +20,34 @@ mongoose.connect(
   }
 );
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute)
-app.use("/api/auth", authRoute)
-app.use("/api/posts", postRoute)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Backend is running!");
