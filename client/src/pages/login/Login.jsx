283d70baec539cd6d50eba773095989a7f873e4a
KlausSkovsmose <klaus.skovsmose@gmail.com>
2021-12-12 14:36:43 +0100

    Use context api on login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,25 +1,64 @@
+import { useRef, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { loginCall } from "../../apiCalls";
 
-import './login.css'
+import {CircularProgress} from "@material-ui/core";
+import "./login.css";
+
 const Login = () => {
-    return (
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Social M</h3>
-                    <span className="loginDesc">Connect with friends and the world around you</span>
-                </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input type="Email" placeholder="Email" className="loginInput" />
-                        <input type="Password" placeholder="Password"className="loginInput" />
-                        <button className="loginButton">Log In</button>
-                        <span className="loginForgotPsw">Forgot Password?</span>
-                        <button className="loginRegisterButton">Create a new Account</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const emailRef = useRef();
+  const pswRef = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: emailRef.current.value, password: pswRef.current.value },
+      dispatch
+    );
+  };
+
+  console.log(user);
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Social M</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleSubmit}>
+            <input
+              type="Email"
+              type="email"
+              placeholder="Email"
+              className="loginInput"
+              ref={emailRef}
+              required
+            />
+            <input
+              type="Password"
+              type="password"
+              placeholder="Password"
+              className="loginInput"
+              ref={pswRef}
+              required
+              minLength="6"
+            />
+            <button className="loginButton">
+              {isFetching ? <CircularProgress color="white" size="23px"/> : "Log in" }
+            </button>
+            <span className="loginForgotPsw">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              Create a new Account
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
